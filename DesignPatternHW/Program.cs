6b3ac2a7b99d3dcf3c05516873e8ead6b78ce8c2
Ethan Vazquez <Ethan.Vazquez050@gmail.com>
2024-10-15 13:21:08 -0700

    Add project files.

---
+++ DesignPatternHW/Program.cs
@@ -0,0 +1,27 @@
+﻿// Singleton Pattern
+// Creational
+/*  Singleton Patterns, in a real project, are usually used to create a private constructor,
+     a private static variable or static method.*/ 
+
+
+public sealed class singleton
+{
+    private static singleton instance = null;
+
+    private singleton()
+    {
+
+    }
+
+    public static singleton Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new singleton();
+            }
+            return instance;
+        }
+    }
+}
